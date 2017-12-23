540a19d4c0d5e8928e61ae4c717f9e68888ca2ab
Hin <hunguet1471994@gmail.com>
2017-12-23 14:58:40 +0700

    INIT FOLDER

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './containers/';
+import './unit/const';
+import './control';
+import { subscribeRecord } from './unit';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+subscribeRecord(store); //Ghi lại trạng thái vào localstorage
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
+
